313458fc4c4aa31753a422164025ca1e0b7ac02f
Adam1618 <mohamed.adam.jemal@gmail.com>
2024-12-01 13:42:36 +0100

    Dashboard header and pages edits

---
+++ src/routes/sidebar.js
@@ -0,0 +1,26 @@
+/**
+ * ⚠ These are used just to render the Sidebar!
+ * You can include any link here, local or external.
+ *
+ * If you're looking to actual Router routes, go to
+ * `routes/index.js`
+ */
+const routes = [
+  {
+    path: '/app/dashboard', // the url
+    icon: 'HomeIcon', // the component being exported from icons/index.js
+    name: 'Access Log', // name that appear in Sidebar
+  },
+  {
+    path: '/app/forms',
+    icon: 'FormsIcon',
+    name: 'Add User',
+  },
+  {
+    path: '/app/tables',
+    icon: 'TablesIcon',
+    name: 'Users',
+  },
+]
+
+export default routes
